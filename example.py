bb9ebbf922850da0a09621664d5ce3dd6c87753e
NK-kimiya <kinarishige26@gmail.com>
2024-11-30 18:30:11 +0900

    .ignoreを追加

--- example.py
+++ example.py
@@ -1,8 +1,4 @@
-#読み込み
-with open('example.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
-    print(text)
-
-#書き込み
-with open('example.txt', 'w', encoding='utf-8') as f:
-    f.write('Hello write method')
+import  pandas as pd
+csv = pd.read_csv('example.csv',encoding='utf-8')
+print(csv)
+print(csv['grade'])
