214a9d9e4d088ef917ce066605389ffff2e04917
NK-kimiya <kinarishige26@gmail.com>
2024-11-30 18:35:36 +0900

    ヘッダーが存在しないcsvファイルを読み込んで、ヘッダーを作成

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
 import  pandas as pd
-csv = pd.read_csv('example.csv',encoding='utf-8')
+csv = pd.read_csv('example.csv',names=('name','grade'),header=None,encoding='utf-8')
 print(csv)
-print(csv['grade'])
+
