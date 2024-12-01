6f00c30c13818e7ac3bd13b397ccb971626a75a3
NK-kimiya <kinarishige26@gmail.com>
2024-12-01 18:41:06 +0900

    品詞を指定して単語を分割

--- example.py
+++ example.py
@@ -1,5 +1,8 @@
-from  janome.tokenizer import Tokenizer
-text='彼女と国立美術館へ行った。'
-t = Tokenizer(wakati=True)
-for token in t.tokenize(text):
+from janome.analyzer import Analyzer
+from janome.tokenfilter import POSKeepFilter
+
+text = '彼女と国立美術館へ言った。'
+token_filters = [POSKeepFilter('名詞')]
+a = Analyzer(token_filters=token_filters)
+for token in a.analyze(text):
     print(token)
