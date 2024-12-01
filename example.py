f1e25c26d8943c3c02d8095538efc6d0c932899c
NK-kimiya <kinarishige26@gmail.com>
2024-12-01 18:19:18 +0900

    htmlからテキストだけを抽出

--- example.py
+++ example.py
@@ -1,3 +1,15 @@
-from  pathlib import  Path
-p = Path('dir1')
-print(list(p.glob('**/*.txt')))
+html = """"
+<html>
+    <body>
+        これは<a href="http://example.com">Example</a>です
+    </body>
+</html>"""
+
+from  bs4 import  BeautifulSoup
+
+def crean_html(html,strip=False):
+    soup = BeautifulSoup(html,'html.parser')
+    text = soup.get_text(strip=strip)
+    return  text
+
+print(crean_html(html))
