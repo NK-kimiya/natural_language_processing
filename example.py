51dba9f770d9ab4ea043b3ff1df3ef470ee65f03
NK-kimiya <kinarishige26@gmail.com>
2024-12-02 22:13:49 +0900

    単語のID化

--- example.py
+++ example.py
@@ -5,5 +5,13 @@ with open('ja.text8','r',encoding='utf-8') as f:
 
 from  collections import Counter
 fdist = (Counter(words))
-print(fdist.most_common(n=10))
 
+UNK='<UNK>'
+PAD='<PAD>'
+vocab = {PAD: 0,UNK: 1}
+for word, _ in fdist.most_common():
+    vocab[word] = len(vocab)
+
+words = ['私','は','元気']
+word_ids = [vocab.get(w,vocab[UNK]) for  w in  words]
+print(word_ids)
